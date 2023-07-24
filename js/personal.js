e6bed993937f9d4ffb181e6f002697cc40ba5638
diagnosticacenter <centerdiagnostica@gmail.com>
2023-07-23 23:17:45 -0500

    index fixes

--- js/personal.js
+++ js/personal.js
@@ -23,7 +23,7 @@ function changeImageBasedOnScreenWidth() {
     if (windowWidth <= 768) {
         image2.src = mobileImagePath2;
         image1.src = mobileImagePath1;
-    
+
         image3.src = mobileImagePath3;
     } else {
         image1.src = desktopImagePath1;
@@ -45,4 +45,33 @@ function toggleMenu() {
 function closeMenu() {
     var navMobile = document.querySelector(".nav-mobile");
     navMobile.style.display = "none";
+}
+
+
+function validateAndSubmit() {
+    const emailInput = document.getElementById("emailInput");
+    const emailWarning = document.getElementById("emailWarning");
+    const messageInput = document.getElementById("messageInput");
+    const messageWarning = document.getElementById("messageWarning");
+    const submitButton = document.getElementById("submitButton");
+
+    const email = emailInput.value.trim();
+    const message = messageInput.value.trim();
+    const words = message.split(/\s+/).filter(word => word !== "");
+
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isMessageValid = words.length >= 3;
+
+    emailWarning.style.display = isEmailValid ? "none" : "block";
+    messageWarning.style.display = isMessageValid ? "none" : "block";
+
+    if (isEmailValid && isMessageValid) {
+        // Aquí puedes agregar la lógica para enviar el formulario
+        // Por ejemplo, podrías hacer una llamada AJAX o realizar otras acciones.
+        const subject = "Consulta";
+        const body = `${encodeURIComponent(message)}%0A%0AContactar al siguiente correo: ${email}`;
+        const recipients = "labdiagnostica@outlook.com,centerdiagnostica@gmail.com";
+        const mailtoLink = `mailto:${recipients}?subject=${encodeURIComponent(subject)}&body=${body}`;
+        window.open(mailtoLink);
+    }
 }
