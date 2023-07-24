3adbb725211e0c6379568a2cab13f7feb8e33a0b
diagnosticacenter <centerdiagnostica@gmail.com>
2023-07-24 04:48:25 -0500

    submenu in index

--- js/index.js
+++ js/index.js
@@ -7,3 +7,29 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuParent = document.querySelector('.menu-parent');
+    const subMenu = menuParent.querySelector('.sub-menu');
+
+    // Agregar evento click al elemento con la clase "menu-parent"
+    menuParent.addEventListener('click', function(event) {
+        event.stopPropagation(); // Evitar que el clic en "Más" cierre el submenú
+
+        // Alternar la visibilidad del submenú al hacer clic en "menu-parent"
+        if (subMenu.style.display === 'block') {
+            subMenu.style.display = 'none';
+        } else {
+            subMenu.style.display = 'block';
+        }
+    });
+
+    // Agregar evento click para cerrar el submenú "Más" al hacer clic en cualquier otro lugar de la página
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+
+        if (!menuParent.contains(target)) {
+            subMenu.style.display = 'none';
+        }
+    });
+});
