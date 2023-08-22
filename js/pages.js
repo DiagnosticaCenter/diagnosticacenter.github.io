10938f349221802731096eca51d01576351336f0
GeovannyBC <140227872+DiagnosticaCenter@users.noreply.github.com>
2023-08-21 21:29:06 -0500

    new img

--- js/pages.js
+++ js/pages.js
@@ -5,32 +5,35 @@ window.onresize = changeImageBasedOnScreenWidth;
 
 function changeImageBasedOnScreenWidth() {
     try {
-    var image1 = document.getElementById('imagebanner1');
-    var mobileImagePath1 = 'images/page-banner-m.jpg';
-    var desktopImagePath1 = 'images/page-banner.jpg';
+        var image1 = document.getElementById('imagebanner1');
+        var mobileImagePath1 = 'images/page-banner-m.jpg';
+        var desktopImagePath1 = 'images/page-banner.jpg';
+        var desktopOImagePath1 = 'images/page-banner-o.jpg';
 
-    var image2 = document.getElementById('imagebanner2');
-    var mobileImagePath2 = 'images/page-banner2-m.jpg';
-    var desktopImagePath2 = 'images/page-banner2.jpg';
+        var image2 = document.getElementById('imagebanner2');
+        var mobileImagePath2 = 'images/page-banner2-m.jpg';
+        var desktopImagePath2 = 'images/page-banner2.jpg';
 
-    var image3 = document.getElementById('imagebanner3');
-    var mobileImagePath3 = 'images/page-banner3-m.jpg';
-    var desktopImagePath3 = 'images/page-banner3.jpg';
-    // Obtener el ancho de la ventana del navegador
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        var image3 = document.getElementById('imagebanner3');
+        var mobileImagePath3 = 'images/page-banner3-m.jpg';
+        var desktopImagePath3 = 'images/page-banner3.jpg';
+        // Obtener el ancho de la ventana del navegador
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    // Establecer la ruta de la imagen en función del ancho de la ventana
-    if (windowWidth <= 768) {
-        image2.src = mobileImagePath2;
-        image1.src = mobileImagePath1;
-        image3.src = mobileImagePath3;
-
-    } else {
-        image1.src = desktopImagePath1;
-        image2.src = desktopImagePath2;
-        image3.src = desktopImagePath3;
-     }
-    }catch (e) {
+        // Establecer la ruta de la imagen en función del ancho de la ventana
+        console.log(windowWidth)
+        if (windowWidth <= 768) {
+            image1.src = mobileImagePath1;
+            image2.src = mobileImagePath2;
+            image3.src = mobileImagePath3;
+        } else if (windowWidth <= 1220 && windowWidth >= 900 ) {
+            image1.src = desktopOImagePath1;
+        } else {
+            image1.src = desktopImagePath1;
+            image2.src = desktopImagePath2;
+            image3.src = desktopImagePath3;
+        }
+    } catch (e) {
     }
 }
 
